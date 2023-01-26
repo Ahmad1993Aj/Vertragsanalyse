55077a478699d60b17c687f82e07c2191a3c09fd
Pokhrel, Bipul Mani <Bipul-Mani.Pokhrel@adesso.de>
2023-01-27 00:33:42 +0100

    Added Additional functions and new Notebook

--- LayoutParser/layoutparser_pipeline.py
+++ LayoutParser/layoutparser_pipeline.py
@@ -8,9 +8,11 @@ Created on Wed Dec 21 13:19:18 2022
 import layoutparser as lp
 import cv2
 import os
+import re
 import sys
 import logging
 import pdf2image
+import pandas as pd
 
 
 class PDF_PreProcessing:
@@ -19,8 +21,9 @@ class PDF_PreProcessing:
     def __init__(self, pdf_file_path, output_path, 
                  model_architecture=None,
                  extra_config=None,
-                 label_map = None,
-                 logger=None):
+                 label_map=None,
+                 logger=None,
+                 labels_of_interest=["Text", "Title"]):
         
         self.pdf_file_path = pdf_file_path
         self.output_path = output_path
@@ -29,25 +32,79 @@ class PDF_PreProcessing:
         self.model_architecture = model_architecture
         self.extra_config = extra_config
         self.label_map = label_map
+        self.loi = labels_of_interest
         
-        self.logger = logging.getLogger("pdfLogger") if logger==None else logger
+        self.images_outputs = dict()
+        
+        if logger==None:
+            logging.basicConfig(filename=self.output_path+"logfile.txt",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+
+            logging.info("Layout Parser Pipeline")
+
+            self.logger = logging.getLogger('pdfLogger')
+        else:
+            self.logger = logger
         
     def scan_images(self):
         """
         This function creates layout of the images given a specific model and a
-        label_map
-
+        generates a dictionary with the follwoing information.
+            *Coordinates:
+            *text
+            *id
+            *type
+            *parent
+            *next
+            *score
         Returns
         -------
-        None.
-
         """
         
         images = self.getImages()
+        result = {"image_path":[],
+                  "page": [],
+                  "page_elem":[],
+                  "x_1":[],
+                  "x_2":[],
+                  "y_1":[],
+                  "y_2":[],
+                  "text":[],
+                  "id":[],
+                  "type":[],
+                  "parent":[],
+                  "next":[],
+                  "score":[],
+                  }
         
-        for image in images:
-            pass
-    
+        for p, image_path in enumerate(images):
+            print("Image path: {}".format(image_path))
+            image = cv2.imread(image_path)
+            image_layout = self.model.detect(image)
+            filtered_layout = lp.Layout([b for b in image_layout if b.type in self.loi])
+ 
+            for i, layout in enumerate(filtered_layout):
+                
+                result["image_path"].append(image_path)
+                result["page"].append(p)
+                result["page_elem"].append(i)
+                result["x_1"].append(layout.block.x_1)
+                result["x_2"].append(layout.block.x_2)
+                result["y_1"].append(layout.block.y_1)
+                result["y_2"].append(layout.block.y_2)
+                result["text"].append(layout.text)
+                result["id"].append(layout.id)
+                result["type"].append(layout.type)
+                result["parent"].append(layout.parent)
+                result["next"].append(layout.next)
+                result["score"].append(layout.score)
+                result
+                
+        self.images_outputs = result
+
     def convert_pdf_to_images(self):
         """
         This function converts a given pdf file to set of jpg images and
@@ -62,24 +119,24 @@ class PDF_PreProcessing:
         if not(os.path.exists(self.pdf_file_path)):
             self.logger.ERROR("PDF file in the given path {} does not exist..."\
                           .format(self.pdf_file_path))
-            self.logger.INFO("Exiting the program...")
+            self.logger.info("Exiting the program...")
             sys.exit(1)
         
         # Create output dictionary if it does not exist
         if not(os.path.exists(self.output_path)):
-            self.logger.INFO("Output path {} does not exist..."\
+            self.logger.info("Output path {} does not exist..."\
                           .format(self.output_path))
             os.mkdir(self.output_path)
-            self.logger.INFO("Created the output directory {} ..."\
+            self.logger.info("Created the output directory {} ..."\
                              .format(self.output_path))
 
         images = pdf2image.convert_from_path(self.pdf_file_path)
-        self.logger.INFO("Converted the images...")
+        self.logger.info("Converted the images...")
 
         for i in range(len(images)):   
             # Save pages as images in the pdf
-            images[i].save(self.output_path+'page'+ str(i) +'.jpg', 'JPEG')
-        self.logger.INFO("Stored the images in the given path {} ...".format(self.output_path))
+            images[i].save(self.output_path+"page"+str(i) +'.jpg', 'JPEG')
+        self.logger.info("Stored the images in the given path {} ...".format(self.output_path))
     
     def load_detectron(self):
         """
@@ -93,32 +150,59 @@ class PDF_PreProcessing:
         self.model = lp.Detectron2LayoutModel(self.model_architecture,
                                  extra_config=self.extra_config,
                                  label_map=self.label_map)
-        
+        self.logger.info("Loaded Detectron Model...")
+
     def getImages(self):
         """
-        
-
         Returns
         -------
         Images : List
             Contains list of the images
         """
-        Images = []
-        return Images
-
-    
-class PDF_PostProcessing:
-    """
-    """
-    def __init__(self):
-        pass
+        Images = os.listdir(self.output_path)
+        result = []
+        for i, img in enumerate(Images):
+            if img[-4:] == ".jpg":
+                result.append(self.output_path + img)
+        def natural_key(string_):
+            """See https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/"""
+            return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+        result = sorted(result, key=natural_key)
+        return result
+        
+    def get_Output(self):
+        """
+        Returns
+        -------
+        Image Outputs : dict
+            Contains list of the output of the layouts
+        """
+        return self.images_outputs
+        
+    def run(self):
+        """
+        This method is a simpe pipeline of the PreProcessing class.
+        The steps are the following one:
+        
+        1. Convert PDF to a collection of Images
+        2. Load the detectron model
+        3. Generate layouts of the images
+        4. Store the layouts as a csv file in the output path
+        Returns
+        -------
+        """
+        self.convert_pdf_to_images()
+        self.load_detectron()
+        self.scan_images()
     
 class OCR:
     """
     """
-    def __init__(self):
+    def __init__(self, input_dict):
+        
         self.agent = None
-    
+        self.output_dict = input_dict
+        
     def load_agent(self):
         """
         Loads the ocr agent.
@@ -128,6 +212,58 @@ class OCR:
         None.
 
         """
-        self.agent = lp.GCVAgent.with_credential("<path/to/your/credential>",
-                                       languages = ['en'])
+        self.agent = lp.TesseractAgent(languages="eng")
         
+    def run(self):
+    	self.load_agent()
+    	self.output_dict["content"] = []
+    	for i, image_path in enumerate(self.output_dict["image_path"]):
+
+            x_1 = float(self.output_dict["x_1"][i])
+            x_2 = float(self.output_dict["x_2"][i])
+            y_1 = float(self.output_dict["y_1"][i])
+            y_2 = float(self.output_dict["y_2"][i])
+            text = self.output_dict["text"][i]
+            row_id = self.output_dict["id"][i]
+            row_type = self.output_dict["type"][i]
+            parent = self.output_dict["parent"][i]
+            next = self.output_dict["next"][i]
+            score = float(self.output_dict["score"][i])
+            
+            block = lp.TextBlock(block=lp.Rectangle(x_1=x_1, y_1=y_1, x_2=x_2, y_2=y_2), text=text, id=row_id, type=row_type, parent=parent, next=next, score=score)
+
+            image = cv2.imread(image_path)
+            
+            segment_image = (block.pad(left=5, right=5, top=5, bottom=5).crop_image(image))
+            
+            text = self.agent.detect(segment_image)
+            self.output_dict["content"].append(text)
+            
+    def get_Output(self):
+        """
+        Returns
+        -------
+        Output : dict
+            Contains list of the output and the ocr content of the layouts
+        """
+        return self.output_dict
+
+class PDF_PostProcessing:
+    """
+    """
+    def __init__(self, output_option, save_dir, input_dict):
+        self.output_option = output_option
+        self.save_dir = save_dir
+        self.output_dict = input_dict
+        
+    def get_OCR_Output(self):
+        pass
+        
+    def get_NLP_Output(self):
+        pass
+        
+    def get_Output(self):
+        pass
+        
+    def run(self):
+        pass
--- LayoutParser/pipeline.py
+++ LayoutParser/pipeline.py
@@ -5,8 +5,72 @@ Created on Wed Dec 21 17:50:01 2022
 @author: pokhrel
 """
 
-import layoutparser_pipeline
+from layoutparser_pipeline import PDF_PreProcessing, OCR, PDF_PostProcessing
+from transformers import pipeline
+from transformers import FSMTForConditionalGeneration, FSMTTokenizer
 
+def summarizer(df, column):
+    classifier = pipeline("summarization", device='cpu')
+    df["summarization"]=df[column].apply(lambda x: summarizer(x))
+    return df
+    
+def question_answering(df, column, question):
+    oracle = pipeline(model="deepset/roberta-base-squad2")
+    df["answer_dict"] = df[column].apply(lambda x: oracle(question=question, context=x))
+    return df
+    
+def translation_to_eng(df, column):
+    mname = "facebook/wmt19-de-en"
+    tokenizer = FSMTTokenizer.from_pretrained(mname)
+    model = FSMTForConditionalGeneration.from_pretrained(mname)
+    def apply_translator(sentence):
+        input_ids = tokenizer.encode(sentence, return_tensors="pt")
+        outputs = model.generate(input_ids)
+        decoded_de_en = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return decoded_de_en
+    df["Translated_to_Eng"] = df[column].apply(apply_translator)
+    return df
+    
+def translation_to_ger(df, column):
+    mname = "facebook/wmt19-en-de"
+    tokenizer = FSMTTokenizer.from_pretrained(mname)
+    model = FSMTForConditionalGeneration.from_pretrained(mname)
+    def apply_translator(sentence):
+        input_ids = tokenizer.encode(sentence, return_tensors="pt")
+        outputs = model.generate(input_ids)
+        decoded_de_en = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return decoded_de_en
+    df["Translated_to_Eng"] = df[column].apply(apply_translator)
+    return df
+
+def keysentence_search(df, string, column, threshold):
+    pass
+
+def pipeline_nlp(pdf_file_path, output_path, model_architecture,
+                extra_config, label_map, logger, task):
+    
+    #1. Preprocessing
+    preprocesser = layoutparser_pipeline.PDF_PreProcessing( pdf_file_path=pdf_file_path,
+                                                           output_path=output_path,
+                                                           model_architecture=model_architecture,
+                                                           extra_config=extra_config,
+                                                           label_map=label_map,
+                                                           logger=logger)
+                                                           
+    preprocessor.run()                                                           
+
+    # Get the Layout Outputs
+    layout_dict = preprocessor.get_Output()
+    
+    #2. OCR
+    ocr_module = OCR(input_dict=layout_dict)
+    ocr_module.run()
+    ocr_dict = ocr_module.get_Output()
+    
+    #3. NLP
+    
+    #Convert ocr_dict to ocr_df
+    
 
 def main(_):
     pdf_file_path = None
@@ -17,25 +81,13 @@ def main(_):
     label_map = None
     logger = None
     
-    preprocesser = layoutparser_pipeline.PDF_PreProcessing(pdf_file_path=pdf_file_path,
-                                                           output_path=output_path,
-                                                           model_architecture=model_architecture,
-                                                           extra_config=extra_config,
-                                                           label_map=label_map,
-                                                           logger=logger)
-    # Load the model
-    preprocesser.load_detectron()
+    task = None
+    task_list = ["Summarization", "Question-Answering", "Translation", "Key Sentence Search"]
     
-    # Convert pdf to images
-    preprocesser.convert_pdf_to_images()
+    if task in task_list:
+        pipeline_nlp(pdf_file_path, output_path, model_architecture,
+                extra_config, label_map, logger, task)
     
-    # Generate layouts
-    preprocesser.scan_images()
-    
-    # Image blocks
-    image_blocks = preprocesser.get_images_blocks()
-    
-    print(image_blocks)
 
 if __name__ == "__main__":
     main()
